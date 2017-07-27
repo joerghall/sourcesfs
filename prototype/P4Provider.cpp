a0eb25c97eecb0727142b05919206070b99868c6
Joerg Hallmann <joerghall@users.noreply.github.com>
2017-07-27 10:19:53 -0700

    p4 provider

--- prototype/P4Provider.cpp
+++ prototype/P4Provider.cpp
@@ -57,7 +57,13 @@ P4Provider::P4Provider(const std::string& repositoryUrl, const std::string& comm
     , _commitNumber(commitNumber)
     , _workingDirectory(workingDirectoryRoot)
 {
+    if(fs::exists(_workingDirectory))
+    {
+        std::cout << "remove " << _workingDirectory << std::endl;
+        fs::remove_all(_workingDirectory);
+    }
 
+    fs::create_directories(_workingDirectory);
 }
 
 P4Provider::~P4Provider()
@@ -67,5 +73,49 @@ P4Provider::~P4Provider()
 
 boost::filesystem::path P4Provider::retrieve(const boost::filesystem::path& path)
 {
-    return "";
+    fs::path target =  _workingDirectory / path;
+    if(fs::exists(target))
+    {
+        return target;
+    }
+
+    std::stringstream fileDir;
+    fileDir << "p4 dirs //" << path.string() << "@" << _commitNumber << " >" << (_workingDirectory / "stdout.txt" ).string() << " 2>" << (_workingDirectory / "stderr.txt" ).string();
+    std::string fileDirCmd = fileDir.str();
+
+    std::stringstream fileSync;
+    fileSync << "p4 print //" << path.string() << "@" << _commitNumber << " >" << target.string() << " 2>" << (_workingDirectory / "stderr.txt" ).string();
+    std::string fileSyncCmd = fileSync.str();
+
+    fs::path pathCache = target.parent_path();
+    if(!fs::exists(pathCache))
+    {
+        std::system(fileSyncCmd.c_str());
+        fs::create_directories(pathCache);
+    }
+
+    std::system(fileSyncCmd.c_str());
+    std::ifstream ifs((_workingDirectory / "stderr.txt").string());
+    std::string stringError((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
+    ifs.close();
+
+    if(stringError.length())
+    {
+        // Check if dir
+        fs::remove(target);
+
+        std::system(fileDirCmd.c_str());
+
+        std::ifstream ifs1((_workingDirectory / "stderr.txt").string());
+        std::string stringError1((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
+        ifs1.close();
+        if(stringError1.length()==0)
+        {
+            fs::create_directories(target);
+            cout << "P4: dir created" << target << endl;
+        }
+    } else {
+        cout << "P4: file created" << target << endl;
+    }
+    return target;
 }
