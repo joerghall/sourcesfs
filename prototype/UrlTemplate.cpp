0240cd89e211c6f88c0cdaed5d638c31110c62ef
Richard Cook <rcook@rcook.org>
2017-07-31 11:23:09 -0700

    Example of how we might expand URL templates

---
+++ prototype/UrlTemplate.cpp
@@ -0,0 +1,91 @@
+// ExpandRepoUrl.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <unordered_map>
+#include <regex>
+#include <string>
+using namespace std;
+
+using path_fragment = string;
+
+struct RepoConfig
+{
+    vector<string> args;
+    string urlTemplate;
+};
+
+struct RepoPathInfo
+{
+    const RepoConfig& repoConfig;
+    string url;
+    vector<path_fragment> pathFragments;
+};
+
+string expandRepoUrlTemplate(const RepoConfig& repoConfig, vector<path_fragment>::const_iterator& iter, vector<path_fragment>::const_iterator& endIter)
+{
+    unordered_map<string, string> argValues;
+    for (const auto& arg : repoConfig.args)
+    {
+        argValues[arg] = *iter++;
+    }
+
+    string url(repoConfig.urlTemplate);
+    for (const auto& pair : argValues)
+    {
+        const auto& arg = pair.first;
+        const auto& argValue = pair.second;
+        const auto token = "${" + arg + "}";
+        const auto findIter = url.find(token);
+        if (findIter != string::npos)
+        {
+            url.replace(findIter, token.size(), argValue);
+        }
+    }
+
+    return url;
+}
+
+RepoPathInfo resolveRepoPathInfo(const unordered_map<string, RepoConfig>& repoConfigs, const vector<path_fragment>& pathFragments)
+{
+    if (pathFragments.size() < 1)
+    {
+        throw runtime_error("Invalid path");
+    }
+
+    auto pathFragmentIter = pathFragments.cbegin();
+    auto pathFragmentEndIter = pathFragments.cend();
+
+    const auto providerName = *pathFragmentIter++;
+    const auto repoConfigIter = repoConfigs.find(providerName);
+    if (repoConfigIter == repoConfigs.cend())
+    {
+        throw runtime_error("Could not find provider \"" + providerName + "\"");
+    }
+
+    const auto& repoConfig = repoConfigIter->second;
+    const auto url = expandRepoUrlTemplate(repoConfig, pathFragmentIter, pathFragmentEndIter);
+
+    return RepoPathInfo
+    {
+        repoConfig,
+        url,
+        { pathFragmentIter, pathFragmentEndIter }
+    };
+}
+
+int main()
+{
+    unordered_map<string, RepoConfig> repoConfigs;
+    repoConfigs.emplace(make_pair<string, RepoConfig>("gitlab", { { "group", "name" }, "git@gitlab.com:${group}/${name}.git" }));
+
+    const auto repoPathInfo = resolveRepoPathInfo(repoConfigs, { "gitlab", "group", "name", "commit", "a", "b", "c" });
+    cout << repoPathInfo.url << endl;
+    for (const auto& pathFragment : repoPathInfo.pathFragments)
+    {
+        cout << "  " << pathFragment << endl;
+    }
+
+    return 0;
+}
